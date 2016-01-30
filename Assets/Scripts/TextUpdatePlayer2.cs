b5f7feb9a2023974a006491d69603a0ecb9378b3
Todor <voiarn@gmail.com>
2016-01-31 00:38:03 +0200

    Added score and ground and background.

---
+++ Assets/Scripts/TextUpdatePlayer2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextUpdatePlayer2 : MonoBehaviour {
+
+    public Text scoreCount;
+    //private Text winText;
+
+    private int count;
+
+    // Use this for initialization
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet1"))
+        {
+            Debug.Log("Collision");
+            count++;
+            SetCountText();
+        }
+    }
+
+
+    void SetCountText()
+    {
+        scoreCount.text = "Score P1: " + count.ToString();
+    }
+}
