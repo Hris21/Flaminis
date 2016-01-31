0977114ec70170bd5c6b99821b66a19c94d2fc87
hris21 <handreev000@gmail.com>
2016-01-31 04:11:46 +0200

    TRANSITION

---
+++ Assets/Scripts/CutsceneTransition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneTransition : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if ()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+    }
+}
